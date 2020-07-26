ea3ebad96d05600242af6260f5775bbf47203341
Jonathan <jawhf4@mail.umkc.edu>
2020-07-26 00:09:34 -0500

    Started Mapping Activity
    - Added Google Map to Mapping Activity

---
+++ Project_Exam_2/Sources/app/src/main/java/com/project/mobile/MappingActivity.java
@@ -0,0 +1,95 @@
+package com.project.mobile;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class MappingActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapping);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap map = googleMap;
+        Geocoder geocoder = new Geocoder(this);
+        StringBuilder userAddress = new StringBuilder();
+
+        final LocationManager userCurrentLocation = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener userCurrentLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) { }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) { }
+
+            @Override
+            public void onProviderEnabled(String s) { }
+
+            @Override
+            public void onProviderDisabled(String s) { }
+        };
+        LatLng userCurrentLocationCorodinates = null;
+        double latitute = 0, longitude = 0;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //show message or ask permissions from the user.
+            return;
+        }
+
+        //Getting the current location of the user.
+        userCurrentLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                userCurrentLocationListener);
+        Location location = userCurrentLocation.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitute = location.getLatitude();
+        longitude = location.getLongitude();
+        userCurrentLocationCorodinates = new LatLng(latitute, longitude);
+
+        //Getting the address of the user based on latitude and longitude.
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitute, longitude, 1);
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName();
+
+            userAddress.append(knownName + ", " + city + ", " + state + ", " + country + " " + postalCode).append("\t");
+            Toast.makeText(this, " " + userAddress, Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //Setting our image as the marker icon.
+        map.addMarker(new MarkerOptions().position(userCurrentLocationCorodinates)
+                .title("Your current address.").snippet(userAddress.toString()));
+        //Setting the zoom level of the map.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocationCorodinates, 7));
+    }
+}
